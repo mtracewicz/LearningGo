5ca0b0b9fca4ef9ddccd936baa1990ad6f92150b
Michał Tracewicz <m.tracewicz@gmail.com>
2021-04-24 13:39:45 +0200

    Added goroutines and benchmarking. Added comandline flags.

--- 16_Mandelbrot/main.go
+++ 16_Mandelbrot/main.go
@@ -1,53 +1,88 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"image"
 	"image/color"
 	"image/png"
 	"math"
 	"os"
+	"runtime"
+	"sync"
+	"time"
 )
 
 func Abs(c complex128) float64 {
 	return math.Sqrt(real(c)*real(c) + imag(c)*imag(c))
 }
 
-func analizeSequence(N int, x, y float64) bool {
+func analyzeSequence(N int, x, y float64) uint8 {
 	z := complex(0, 0)
 	p := complex(x, y)
 	for i := 1; i < N; i++ {
 		z = z*z + p
 		if Abs(z) >= 2.0 {
-			return false
+			return uint8(255 * i / N)
 		}
 	}
-	return true
+	return 255
 }
 
-func visualizeData(size, N int) {
+func createImage(size, N, cpuCount int) (time.Duration, image.Image) {
+	runtime.GOMAXPROCS(cpuCount)
+	workPerCore := 6 * size * size / cpuCount
+
 	upLeft := image.Point{0, 0}
 	lowRight := image.Point{3 * size, 2 * size}
-
 	img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
 
-	for i := 0; i < 2*size; i++ {
-		y := 2.0*float64(i)/float64(2*size) - 1.0
-		for j := 0; j < 3*size; j++ {
-			x := 3.0*float64(j)/float64(3*size) - 2.0
-			if analizeSequence(N, x, y) {
-				img.Set(j, i, color.White)
-			} else {
-				img.Set(j, i, color.Transparent)
+	var wg sync.WaitGroup
+	wg.Add(cpuCount)
+
+	startTime := time.Now()
+	for cpu := 0; cpu < cpuCount; cpu++ {
+		go func(currentCpu int) {
+			start := currentCpu * workPerCore
+			stop := start + workPerCore
+
+			for it := start; it < stop; it++ {
+				i := it % (3 * size)
+				j := it / (3 * size)
+
+				x := 3.0*float64(i)/float64(3*size) - 2.0
+				y := 2.0*float64(j)/float64(2*size) - 1.0
+				a := analyzeSequence(N, x, y)
+				img.Set(i, j, color.RGBA{R: 0xf0, G: 0x3a, B: 0x47, A: a})
 			}
-		}
-
+			wg.Done()
+		}(cpu)
 	}
+	wg.Wait()
+	stopTime := time.Since(startTime)
 
-	// Encode as PNG.
+	return stopTime, img
+}
+
+func visualizeData(size, N, cpuCount int) {
+	fmt.Println("Calculating mandelbrot set with parameters: size =", size, "accuracy =", N, "CPU count = ", cpuCount)
+	_, img := createImage(size, N, cpuCount)
 	f, _ := os.Create("image.png")
 	png.Encode(f, img)
+	fmt.Println("Compleated. Checkout image.png!")
 }
 
 func main() {
-	visualizeData(600, 200)
+	size := flag.Int("size", 60, "Base on which image size will be calculated: Width=3*size, Height=2*size")
+	accuracy := flag.Int("accuracy", 200, "How many iterations take place during calculation for single point")
+	cpuCount := flag.Int("cpuCount", runtime.NumCPU(), "How many goroutines should be used")
+	benchmarkMode := flag.Bool("benchmark", false, "If enabled no image will be generated, instead info about computation time will be provided.")
+	flag.Parse()
+
+	if *benchmarkMode {
+		calculationsTime, _ := createImage(*size, *accuracy, *cpuCount)
+		fmt.Println("size =", *size, "accuracy =", *accuracy, "cpuCount = ", *cpuCount, "calculationsTime =", calculationsTime)
+	} else {
+		visualizeData(*size, *accuracy, *cpuCount)
+	}
 }
